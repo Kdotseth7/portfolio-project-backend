e7547f2c2f1bc680cea31e42f690ca9c25ee0f3d
Kushagra Seth <kseth919@gmail.com>
2020-05-30 18:35:27 +0530

    Incorrect Form Submission validation

--- controllers/people.js
+++ controllers/people.js
@@ -1,5 +1,8 @@
 const peopleHandler = (req, res, db) => {
     const { name, email, subject, message } = req.body;
+    if ( !name || !email || !subject || !message ) {
+        return res.status(400).json("Incorrect Form Submission");
+    }
     db
         .insert({
             name: name,
